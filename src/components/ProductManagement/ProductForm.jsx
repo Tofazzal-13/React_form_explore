691ca0b0972a88a29830ba4e638b14318ef590d4
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-07-13 22:23:05 +0600

    Display form data in a table and form error handle

--- src/components/ProductManagement/ProductForm.jsx
+++ src/components/ProductManagement/ProductForm.jsx
@@ -1,19 +1,45 @@
+import { useState } from "react";
 
 
-const ProductForm = () => {
+const ProductForm = ({handleAddProduct}) => {
+    const [ error , setError] = useState('')
     const handleProductSubmit = e => {
         e.preventDefault();
         const name = e.target.name.value;
         const price = e.target.price.value;
         const quantity = e.target.quantity.value;
         // console.log(name, price, quantity);
+        if(name.length === 0) {
+            setError("please enter your name")
+            return;
+        }
+        else if(price.length === 0){
+            setError("Please fill the price field")
+            return;
+        }
+        else if(price < 0){
+            setError("Price can not negative")
+            return;
+        }
+        else if(quantity.length === 0){
+            setError("Please give a amount");
+            return;
+        }
+        else if(quantity < 0){
+            setError("Quantity can't negetive");
+            return;
+        }
+
+        else{
+            setError('')
+        }
 
         const newProduct = {
             name, 
             price, 
             quantity
         }
-        console.log(newProduct);
+        handleAddProduct(newProduct)
         
         
         
@@ -27,6 +53,7 @@ const ProductForm = () => {
                 <input type="text"  name="quantity" placeholder="Product Quantity"/> <br />
                 <input type="submit" value="Submit" />
             </form>
+            <p style={{color: "red"}}><small> { error } </small></p>
         </div>
     );
 };
--- src/components/ProductManagement/ProductTable.jsx
+++ src/components/ProductManagement/ProductTable.jsx
@@ -1,8 +1,31 @@
 
-const ProductTable = () => {
+const  ProductTable = ({products}) => {
     return (
         <div>
-            
+            <h3>Products: {products.length}</h3>
+
+            <table style={{margin:"0 auto"}}>
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map((product, index) => <tr>
+                            <td>{index + 1}</td>
+                            <td>{product.name}</td>
+                            <td>{product.price}</td>
+                            <td>{product.quantity}</td>
+                            <td></td>
+                        </tr>)
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };

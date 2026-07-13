cb1171addaf350341731f37c1ba8e16cf02c4bad
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-07-13 19:53:31 +0600

    Use Custom hook to reduce duplication of the code

--- src/components/ControlledField/ControlledField.jsx
+++ src/components/ControlledField/ControlledField.jsx
@@ -2,13 +2,29 @@ import { useState } from "react";
 
 
 const ControlledField = () => {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState('')
 
     const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log("");
+        e.preventDefault()  
+        console.log(name, email, password);
+        if(password.length > 6){
+            setError("6 character or longer password needed")
+        }
+        else{
+            setError(" ")
+        }
         
+    }
+
+    const handleEmailOnChange =  (e) =>  {
+        setEmail(e.target.value)
+    }
+
+    const handleNameChange = e => {
+        setName(e.target.value)
     }
 
     const handlePasswordOnChange = (e) =>  {
@@ -18,14 +34,16 @@ const ControlledField = () => {
             setError("password must be 6 char")
           }
           else{
-            setError("")
+            setError(" ")
           }
     }
 
     return (
         <div onSubmit={handleSubmit}>
             <form>
-                <input type="email" name="email"  placeholder="Email" required /> 
+                <input type="text" placeholder="Enter Your Name"  defaultValue={name} onChange={handleNameChange}/>
+                <br />
+                <input type="email" name="email"  placeholder="Email" required onChange={handleEmailOnChange} defaultValue={email}/> 
                 <br />
                 <input type="password" name="password" onChange={handlePasswordOnChange}  defaultValue={password}  placeholder="Password"  required/> 
                 <br />
